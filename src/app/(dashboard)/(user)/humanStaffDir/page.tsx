7b4360da5830fd33dc530f6b4e54a3988eba9f94
Magarey <lokeshmagar507@gmail.com>
2025-01-07 12:09:01 +0545

    correction in files

--- src/app/(dashboard)/(user)/humanStaffDir/page.tsx
+++
@@ -1,7 +0,0 @@
-import StaffDirectoryLayout from '@/views/humanResource/staffDirectory'
-
-const HumanStaffDirectory = () => {
-  return <StaffDirectoryLayout />
-}
-
-export default HumanStaffDirectory
